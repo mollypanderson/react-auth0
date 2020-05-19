4b010177ff878b933d8f8cfd22797e052db530d8
mollypanderson <mollypanderson@yahoo.com>
2020-05-19 13:37:40 -0700

    adding routes

--- react-auth0/src/index.js
+++ react-auth0/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App />,
+    <Router>
+      <Route component={App} />
+    </Router>,
   document.getElementById('root')
 );
